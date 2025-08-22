15bc4a7aeaf9341893e18435de1afee96ef8ba4e
dBajenaru <dani.bajenaru@outlook.com>
2025-08-21 21:57:44 -0400

    returning to the basics. I don't know what I did before so I'm just coming back to a point that I know makes sense. Also, .gitignore

--- main.py
+++ main.py
@@ -9,11 +9,26 @@ def get_book_text(filepath):
 
 def main():
     frankenstein = "books/frankenstein.txt"
-    book = get_book_text(frankenstein)
-    word_count = stats.count_words(book)
-    char_count = stats.count_characters(book)
+    text = get_book_text(frankenstein)
+    word_count = stats.count_words(text)
+    char_count = stats.count_characters(text) # this is char dict
+
+    breakpoint()
+    sorted_chars = stats.sort_characters(char_count)
+#    print(char_count)
+    print(sorted_chars)
+
+    """
+    split_dict = stats.split_dictionary(char_count)
+    print(split_dict)
+    split_dict.sort(reverse=True, key=stats.sort_on)
+    print(split_dict)
+    """
+
+#    sort_chars = stats.sort_characters(char_count)
     print(word_count, "words found in the document")
-    print(char_count)
+#    print(char_count)
+#    print(sort_chars)
 
 
 main()
--- stats.py
+++ stats.py
@@ -14,3 +14,24 @@ def count_characters(text):
             char_count[char] = 1
 
     return char_count
+
+
+# sort() key method
+def sort_on(items):
+    return items["count"]
+
+
+def split_dictionary(dict):
+    split_dict = []
+    for key, value in dict.items():
+        split_dict.append({"char": key, "count": value})
+
+    return split_dict
+
+
+def sort_characters(dict):
+    split_dict = split_dictionary(dict)
+    print(split_dict)
+    sorted = split_dict.sort(reverse=True, key=sort_on)
+
+    return sorted
